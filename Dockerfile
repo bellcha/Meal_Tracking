38af2db5e63d5c079a772c8051e2d4aa9cf5cea2
Bradley Bell <bellcha@gmail.com>
2023-01-16 16:24:29 -0600

    Added docker file

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.10.6
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- testing.py
+++
@@ -1,9 +0,0 @@
-import json
-from food_data import NutritionalAPI
-
-
-y = NutritionalAPI("014100053293")
-
-y_json = json.loads(y.data.json())
-
-print(json.dumps(y_json, indent=4))
